977129ad01201223b2ead649391b53f119894da7
Stephen Mayeux <stephenmayeux@gmail.com>
2016-07-16 12:34:09 +0900

    add meteor method and validation

--- client/main.js
+++ client/main.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
+import LinkCreate from './components/link_create';
+import { Links } from '../imports/collections/links';
 
 const App = () => {
+
   return (
     <div>
       <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <LinkCreate />
+          </div>
+        </div>
+      </div>
     </div>
   );
+
 };
 
 Meteor.startup(() => {
--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,3 +1,17 @@
+/*
+** Because this file isn't in either the client or server dirs,
+** it will not be loaded automatically. We need to import this file
+** in both the main.js files in /clients and /server.
+*/
+
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import {check, Match } from 'meteor/check'; // validation
+
+Meteor.methods({
+  'links.insert': function(url) {
+    check(url, Match.Where(url => validUrl.isUri(url)));
+  }
+});
 
 export const Links = new Mongo.Collection('links');
